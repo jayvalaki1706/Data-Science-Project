eeb10f6f035f4e6d58480741e069cdb9d55dedef
Jay Valaki <jayvalaki2004@gmail.com>
2026-03-14 00:42:52 +0530

    The setup and requirements are done.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ## End to End Data Science Project
 
-Steps
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements= [req.replace("\n", "") for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+            
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Jay",
+    author_email="<jayvalaki2004@gmail.com>",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    )
